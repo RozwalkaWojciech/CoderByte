9d1a03064ec399a89e60cc7fa499ca88cf7e6c30
RozwalkaWojciech <wojciechrozwalka@gmail.com>
2021-02-04 14:37:07 +0100

    FindIntersection another solution

--- src/main/java/pl/rozwalka/coderbyte/findintersection/FindIntersection.java
+++ src/main/java/pl/rozwalka/coderbyte/findintersection/FindIntersection.java
@@ -1,51 +1,34 @@
 package pl.rozwalka.coderbyte.findintersection;
 
-import java.util.Arrays;
-import java.util.HashSet;
 import java.util.Scanner;
 
 class Main {
 
     public static String FindIntersection(String[] strArr) {
-
-        if (strArr.length < 2) {
-            return "false";
-        }
-        if (strArr[0] == null || strArr[1] == null) {
-            return "false";
-        }
-        if (strArr[0].isEmpty() || strArr[1].isEmpty()) {
-            return "false";
-        }
-        if (strArr[0].trim().isBlank() || strArr[1].trim().isBlank()) {
-            return "false";
-        }
-
-        String[] split1 = strArr[0].trim().split(",");
-        String[] split2 = strArr[1].trim().split(",");
+        String str1 = strArr[0];
+        String str2 = strArr[1];
         String result = "";
-
-        HashSet<String> set = new HashSet<>(Arrays.asList(split1));
-        for (int i = 0; i < split2.length; i++) {
-            if (!set.add(split2[i])) {
-                if (result.isBlank()) {
-                    result = split2[i];
-                } else {
-                    result += "," + split2[i].trim();
+        for (String check : str1.split(", ")) {
+            for (String check2 : str2.split(", ")) {
+                if (check.equals(check2)) {
+                    if (result.length() == 0) {
+                        result += check;
+                    } else {
+                        result += "," + check;
+                    }
                 }
             }
         }
-        if (result.isBlank()) {
+        if (result.isEmpty())
             return "false";
-        }
-        return result.trim();
+        return result;
     }
 
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
         String s1 = s.nextLine();
         String s2 = s.nextLine();
-        String[] strArr = {s1,s2};
+        String[] strArr = {s1, s2};
         System.out.print(FindIntersection(strArr));
     }
 }
