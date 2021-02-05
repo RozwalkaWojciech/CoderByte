73ac9384158a4a174df147492ca3721871bf7374
RozwalkaWojciech <wojciechrozwalka@gmail.com>
2021-02-05 11:58:58 +0100

    ABCheck

--- src/main/java/pl/rozwalka/coderbyte/ab/check/Main.java
+++ src/main/java/pl/rozwalka/coderbyte/ab/check/Main.java
@@ -1,6 +1,7 @@
 package pl.rozwalka.coderbyte.ab.check;
 
 import java.util.Scanner;
+import java.util.regex.Pattern;
 
 class Main {
 
@@ -8,6 +9,17 @@ class Main {
 
         String s = str.toLowerCase();
 
+        Pattern compile1 = Pattern.compile("a...b");
+        Pattern compile2 = Pattern.compile("b...a");
+
+        boolean matcher1 = compile1.matcher(str).find();
+        boolean matcher2 = compile2.matcher(str).find();
+
+        if (matcher1 || matcher2){
+            return true;
+        }
+
+//      *****************  another way  ********************
         for (int i = 0; i < s.length(); i++) {
             if (i + 4 < s.length() && s.charAt(i) == 'a' && s.charAt(i + 4) == 'b'
                     || i + 4 < s.length() && s.charAt(i) == 'b' && s.charAt(i + 4) == 'a'
