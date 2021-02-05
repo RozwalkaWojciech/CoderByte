82ca6ed2557e285b02ca40be5ae33e38d0e5cd3c
RozwalkaWojciech <wojciechrozwalka@gmail.com>
2021-02-05 21:46:52 +0100

    MeanMode

---
+++ src/main/java/pl/rozwalka/coderbyte/mean/mode/Main.java
@@ -0,0 +1,33 @@
+package pl.rozwalka.coderbyte.mean.mode;
+
+/* Using the Java language, have the function MeanMode(arr)
+take the array of numbers stored in arr and return 1 if the mode equals the mean,
+0 if they don't equal each other (ie. [5, 3, 3, 3, 1] should return 1 because the mode (3) equals the mean (3)).
+The array will not be empty, will only contain positive integers, and will not contain more than one mode. */
+
+class Main {
+
+    public static int MeanMode(int[] arr) {
+
+        int sum = 0;
+        int avg = 0;
+
+        for (int i = 0; i < arr.length; i++) {
+            sum += arr[i];
+        }
+        avg = sum / arr.length;
+        for (int i = 0; i < arr.length; i++) {
+            if (avg == arr[i]) {
+                return 1;
+            }
+        }
+        return 0;
+    }
+
+    public static void main(String[] args) {
+
+        int[] arr = {5, 3, 3, 3, 1};
+
+        System.out.println(MeanMode(arr));
+    }
+}
