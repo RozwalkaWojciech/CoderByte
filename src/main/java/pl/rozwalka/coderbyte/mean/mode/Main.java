6c3f0b7fdf345f63644e62b4317eaa455696778f
RozwalkaWojciech <wojciechrozwalka@gmail.com>
2021-02-07 12:40:11 +0100

    MeanMode

--- src/main/java/pl/rozwalka/coderbyte/mean/mode/Main.java
+++ src/main/java/pl/rozwalka/coderbyte/mean/mode/Main.java
@@ -10,12 +10,13 @@ class Main {
     public static int MeanMode(int[] arr) {
 
         int sum = 0;
-        int avg = 0;
 
         for (int i = 0; i < arr.length; i++) {
             sum += arr[i];
         }
-        avg = sum / arr.length;
+
+        int avg = sum / arr.length;
+
         for (int i = 0; i < arr.length; i++) {
             if (avg == arr[i]) {
                 return 1;
